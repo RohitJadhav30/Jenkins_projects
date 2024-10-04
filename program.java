87fb9c3ff076fe3d12fb9548685de1b14d30a1b2
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-10-04 14:17:05 +0530

    first jenkins java project

---
+++ program.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class program{
+    public static void main(String[] args){
+        System.out.println("This is java program using jenkins");
+        System.out.println(new Date());
+    }
+}
