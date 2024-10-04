9a55c46725a35b6295b7fed56180dd9068affc98
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-10-04 14:29:09 +0530

    Making chnages

--- program.java
+++ program.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class program{
+public class Program{
     public static void main(String[] args){
         System.out.println("This is java program using jenkins");
         System.out.println(new Date());
