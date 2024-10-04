f5a6c65dac2fcf5cc0df34e55dde037e9b26a2c5
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-10-04 14:35:17 +0530

    added one more line

--- program.java
+++ program.java
@@ -4,5 +4,6 @@ public class Program{
     public static void main(String[] args){
         System.out.println("This is java program using jenkins");
         System.out.println(new Date());
+        System.out.println("added one more line");
     }
 }
