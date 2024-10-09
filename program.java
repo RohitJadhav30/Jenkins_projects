f34ecc76f734a03dbf50aecbe3948f30fb8ef3e9
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-10-09 11:20:23 +0530

    Chnages

--- program.java
+++ program.java
@@ -5,6 +5,6 @@ public class Program{
         System.out.println("This is java program using jenkins");
         System.out.println(new Date());
         System.out.println("added one more line");
-        System.out.println("This is jenkins project");
+        
     }
 }
